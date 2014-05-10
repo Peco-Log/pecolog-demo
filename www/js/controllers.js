19ce3ad7db09f9b4a9b5aed5f8dcdc50747a8658
UGA <s.ugagod@gmail.com>
2014-05-11 01:24:56 +0900

    show contents

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,11 +1,11 @@
 angular.module('ionic.example', ['ionic'])
 
-.controller('MapCtrl', function($scope, $http, $ionicLoading) {
+.controller('MapCtrl', function($scope, $http, $ionicLoading, $ionicActionSheet) {
   function initialize() {
     $http.get("https://api-datastore.appiaries.com/v1/dat/_sandbox/pecolog/shop/-;").success(function(json){
       var data = new Array();
-      data.push({ z: json._objs[0]._coord[1],   x: json._objs[0]._coord[0],   content: json._objs[0].name});
-      data.push({ z: json._objs[1]._coord[1],   x: json._objs[1]._coord[0],   content: json._objs[1].name});
+      data.push({ z: json._objs[0]._coord[1],   x: json._objs[0]._coord[0],   content: json._objs[0].name,  comment: "うまい！"});
+      data.push({ z: json._objs[1]._coord[1],   x: json._objs[1]._coord[0],   content: json._objs[1].name,  comment: "まずい！"});
       var latlng = new google.maps.LatLng(json._objs[0]._coord[1], json._objs[0]._coord[0]);
       var mapOptions = {
         center: latlng,
@@ -28,7 +28,8 @@ angular.module('ionic.example', ['ionic'])
           icon: img
         });
         var infowindow = new google.maps.InfoWindow({
-          content: "<div class='message'>" + data[i].content + "</div>",
+          //content: "<div class='message'>" + data[i].content + "</div>",
+          content: data[i].content + ":" + data[i].comment,
           position: new google.maps.LatLng(data[i].z, data[i].x),
           //position: data[i].position
           //size: new google.maps.Size(50, 50)
@@ -49,9 +50,30 @@ angular.module('ionic.example', ['ionic'])
   }
   google.maps.event.addDomListener(window, 'load', initialize);
 
-  function attachMessage(marker, infowindow) {
+  function attachMessage(marker, infowindow, ionicActionSheet) {
     google.maps.event.addListener(marker, "click", function() {
-      infowindow.open(marker.getMap(), marker);
+      var contents = infowindow.getContent().split(":");
+      $ionicActionSheet.show({
+        titleText: contents[0],
+        buttons: [
+          { text: contents[1]},
+          //{ text: 'Share <i class="icon ion-share"></i>' },
+          //{ text: 'Move <i class="icon ion-arrow-move"></i>' },
+        ],
+        //destructiveText: 'Delete',
+        //cancelText: 'Cancel',
+        //cancel: function() {
+        //  console.log('CANCELLED');
+        //},
+        //buttonClicked: function(index) {
+        //  console.log('BUTTON CLICKED', index);
+        //  return true;
+        //},
+        destructiveButtonClicked: function() {
+          console.log('DESTRUCT');
+          return true;
+        }
+      });
     });
   }
 
